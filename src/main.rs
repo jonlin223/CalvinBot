80892cf7f5d6889a5eb369d208eaeba645881ddc
Jonathan Lin <jonlin223@gmail.com>
2023-08-21 16:58:59 +1000

    Test ping command

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
 use std::env;
 
-use serenity::{framework::StandardFramework, prelude::GatewayIntents};
+use serenity::{
+    async_trait,
+    framework::{StandardFramework, standard::{macros::{group, command}, CommandResult}},
+    prelude::{EventHandler, GatewayIntents, Context},
+    Client, model::prelude::Message,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[group]
+#[commands(ping)]
+struct General;
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("!!"));
+    dotenv::dotenv().expect("Failed to load .env file");
+
+    let framework = StandardFramework::new().configure(|c| c.prefix("!!")).group(&GENERAL_GROUP);
 
     let token = env::var("TOKEN").expect("token");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(e) = client.start().await {
+        println!("An error occurred while running the client: {:?}", e);
+    }
 }
