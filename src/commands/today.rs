bc98150b0c11d7873ef78a6a3ba75fabc06e136a
Jonathan Lin <jonlin223@gmail.com>
2023-08-22 16:51:40 +1000

    Implement basic version of scraper

--- src/commands/today.rs
+++ src/commands/today.rs
@@ -4,9 +4,12 @@ use crate::scrapers::calvin_scraper;
 
 #[command]
 async fn today(ctx: &Context, msg: &Message) -> CommandResult {
-    let res = calvin_scraper::scrape("2023/100/22").await;
+    let res = calvin_scraper::scrape("2023/08/22").await;
     match res {
-        Ok(_) => println!("HI THERE"),
+        Ok(x) => {
+            println!("{x}");
+            msg.reply(ctx, x).await?;
+        },
         Err(_) => println!("Oh no")
     }
     Ok(())
--- src/scrapers/calvin_scraper.rs
+++ src/scrapers/calvin_scraper.rs
@@ -2,9 +2,13 @@ use reqwest::Error;
 
 pub async fn scrape(date: &str) -> Result<String, Error>{
 
+    // Get HTML in String
     let url = "https://www.gocomics.com/calvinandhobbes/".to_owned() + date;
-    println!("{url}");
     let res = reqwest::get(url).await?.text().await?;
 
-    Ok(res)
+    let doc = scraper::Html::parse_document(&res);
+    let selector = scraper::Selector::parse("picture.item-comic-image>img").unwrap();
+    let img = doc.select(&selector).map(|x| x.value().attr("src")).next().unwrap().unwrap();
+
+    Ok(img.to_owned())
 }
