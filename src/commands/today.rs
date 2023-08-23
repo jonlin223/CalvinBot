990c209327790c7c9742252b6ff806c143c79519
Jonathan Lin <jonlin223@gmail.com>
2023-08-23 15:18:25 +1000

    Finish today command

--- src/commands/today.rs
+++ src/commands/today.rs
@@ -1,16 +1,25 @@
-use serenity::{framework::standard::{macros::command, CommandResult}, prelude::Context, model::prelude::Message};
+use serenity::{
+    framework::standard::{macros::command, CommandResult},
+    model::prelude::Message,
+    prelude::Context,
+};
 
-use crate::scrapers::calvin_scraper;
+use crate::scrapers::calvin_scraper::{self, get_today};
 
 #[command]
 async fn today(ctx: &Context, msg: &Message) -> CommandResult {
-    let res = calvin_scraper::scrape("2023/08/22").await;
-    match res {
-        Ok(x) => {
-            println!("{x}");
-            msg.reply(ctx, x).await?;
-        },
-        Err(_) => println!("Oh no")
-    }
+    let date = get_today().await;
+    match date {
+        Some(d) => {
+            let url = &("https://www.gocomics.com/calvinandhobbes/".to_owned() + &d);
+            let res = calvin_scraper::scrape(url).await;
+            match res {
+                Some(x) => msg.reply(ctx, x).await?,
+                None => msg.reply(ctx, "Could not find today's comic").await?,
+            }
+        }
+        None => msg.reply(ctx, "Could not find today's comic").await?,
+    };
+
     Ok(())
 }
--- src/scrapers/calvin_scraper.rs
+++ src/scrapers/calvin_scraper.rs
@@ -1,14 +1,35 @@
-use reqwest::Error;
+use scraper::{Html, Selector};
 
-pub async fn scrape(date: &str) -> Result<String, Error>{
+/// Get date string corresponding to today's comic
+pub async fn get_today() -> Option<String> {
+    // Get HTML as string
+    let url = "https://www.gocomics.com/calvinandhobbes/";
+    let res = reqwest::get(url).await.ok()?.text().await.ok()?;
+    let doc = Html::parse_document(&res);
 
+    // Parse for href to today's comic
+    let selector = Selector::parse(r#"a[data-link="comics"]"#).ok()?;
+    let href = doc
+        .select(&selector)
+        .map(|x| x.value().attr("href"))
+        .next()??;
+
+    // Get date
+    let date = href.strip_prefix("/calvinandhobbes/")?;
+
+    Some(date.to_owned())
+}
+
+pub async fn scrape(url: &str) -> Option<String> {
     // Get HTML in String
-    let url = "https://www.gocomics.com/calvinandhobbes/".to_owned() + date;
-    let res = reqwest::get(url).await?.text().await?;
+    let res = reqwest::get(url).await.ok()?.text().await.ok()?;
 
-    let doc = scraper::Html::parse_document(&res);
-    let selector = scraper::Selector::parse("picture.item-comic-image>img").unwrap();
-    let img = doc.select(&selector).map(|x| x.value().attr("src")).next().unwrap().unwrap();
+    let doc = Html::parse_document(&res);
+    let selector = Selector::parse("picture.item-comic-image>img").ok()?;
+    let img = doc
+        .select(&selector)
+        .map(|x| x.value().attr("src"))
+        .next()??;
 
-    Ok(img.to_owned())
+    Some(img.to_owned())
 }
